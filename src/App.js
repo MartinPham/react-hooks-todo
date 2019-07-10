4fb2b30e8cb6894c318370d305d3fadb4d1d7cfe
Martin Pham <tuyentq2009@gmail.com>
2019-07-10 21:08:14 +0200

    play with reducer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
-import React, {useState} from 'react'
+import React, {useReducer, useState} from 'react'
 import TaskList from './TaskList'
 import TaskEditor from './TaskEditor'
 
+const taskReducer = (tasks, action) => {
+  if(action.type === 'ADD')
+  {
+    const newTasks = [...tasks]
+    newTasks.push({
+      id: (new Date()).getSeconds(),
+      name: action.payload.name
+    })
+
+    return newTasks
+  }else if(action.type === 'EDIT')
+  {
+    const newTasks = [...tasks]
+    for(let i = 0; i < newTasks.length; i++)
+    {
+      if(newTasks[i].id === action.payload.id)
+      {
+        newTasks[i].name = action.payload.name
+        break
+      }
+    }
+
+    return newTasks
+  }else if(action.type === 'DELETE')
+  {
+    const newTasks = tasks.filter(task => task.id !== action.payload.id)
+
+    return newTasks
+  }
+  return tasks
+}
+
+const initialTasks = [
+];
 
 function App() {
-  const [tasks, setTasks] = useState([])
+  const [tasks, dispatchTaskAction] = useReducer(taskReducer, initialTasks)
   const [editingTask, setEditingTask] = useState(null)
 
   return (
@@ -12,38 +46,29 @@ function App() {
       <TaskList tasks={tasks} onRequestEdit={task => {
         setEditingTask({...task})
       }} onRequestDelete={id => {
-        setTasks(tasks => {
-          const newTasks = tasks.filter(task => task.id !== id)
-
-          return newTasks
+        dispatchTaskAction({
+          type: 'DELETE',
+          payload: {
+            id
+          }
         })
       }}/>
       <TaskEditor task={editingTask} onSave={task => {
         if(task.id === null)
         {
-          setTasks(tasks => {
-            const newTasks = [...tasks]
-            newTasks.push({
-              id: (new Date()).getSeconds(),
+          dispatchTaskAction({
+            type: 'ADD',
+            payload: {
               name: task.name
-            })
-        
-            return newTasks
+            }
           })
         } else {
-          setEditingTask(null)
-          setTasks(tasks => {
-            const newTasks = [...tasks]
-            for(let i = 0; i < newTasks.length; i++)
-            {
-              if(newTasks[i].id === task.id)
-              {
-                newTasks[i].name = task.name
-                break
-              }
+          dispatchTaskAction({
+            type: 'EDIT',
+            payload: {
+              id: task.id,
+              name: task.name
             }
-        
-            return newTasks
           })
         }
       }}/>
